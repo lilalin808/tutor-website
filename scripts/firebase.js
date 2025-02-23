e37fbd888d1c9bd81da9575ecb6fd444b6a3370a
lilalin808 <lilalanlin@icloud.com>
2025-02-23 01:38:04 -0800

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -2,6 +2,7 @@
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -15,3 +16,27 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+
+
+//submit button
+const submit = document.getElementById("submit");
+submit.addEventListener("click",function(event){
+  event.preventDefault()
+    //inputs
+const email = document.getElementById("email").value;
+const passwrod = document.getElementById("password").value;
+  
+  createUserWithEmailAndPassword(auth,email,password)
+  .then((userCredential) => {
+    //signed up
+    const user = userCredential.user;
+    alert("Signing Up")
+    //...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+    //..
+  });
+})
