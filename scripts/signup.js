f3cd0eccfb85e972f8585d62112e3efeb278dad3
lilalin808 <lilalanlin@icloud.com>
2025-02-23 02:29:16 -0800

    Update signup.js

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,10 +1,9 @@
-// Import necessary Firebase modules (modular imports)
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
-  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
+import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
