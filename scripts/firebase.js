2a5cc0edb96c94e3cfd0c5267fc703cb10d112b9
lilalin808 <lilalanlin@icloud.com>
2025-02-23 01:00:31 -0800

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmS3PF33c4BHzgjKuM0LUSu_wpIFQSNvk",
+    authDomain: "peer-tutor-a1076.firebaseapp.com",
+    projectId: "peer-tutor-a1076",
+    storageBucket: "peer-tutor-a1076.firebasestorage.app",
+    messagingSenderId: "677806357185",
+    appId: "1:677806357185:web:be5149be7ba68343517240"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
